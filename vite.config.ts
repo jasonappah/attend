2ec80c0009b4c60d65c151e7017236bae4c2058a
Jason Antwi-Appah <hey@jasonaa.me>
2024-12-29 16:55:36 -0600

    style: format

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,9 @@ export default {
       // environment variables get loaded from .env by one plugin on init,
       // so i'm hoping that by the time this gets evaluated, one has finished initializing?
       // and by importing the env, we still make sure the env gets validated on build
-      optimize: await import('./src/env').then(src=>src.env).then((env) => env.NODE_ENV === 'production'),
+      optimize: await import('./src/env')
+        .then((src) => src.env)
+        .then((env) => env.NODE_ENV === 'production'),
       components: ['tamagui'],
       config: './src/tamagui/tamagui.config.ts',
       outputCSS: './src/tamagui/tamagui.css',
