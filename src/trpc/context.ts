0186c1ad7831d078d656d43aea7f26e7ffdc9551
Jason Antwi-Appah <hey@jasonaa.me>
2025-01-07 05:36:06 -0700

    style: format

--- src/trpc/context.ts
+++ src/trpc/context.ts
@@ -1,12 +1,17 @@
-import type { FetchCreateContextFnOptions } from '@trpc/server/adapters/fetch'
-import { auth } from '~/better-auth/auth'
-import { db } from '~/db'
+import type { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
+import { auth } from "~/better-auth/auth";
+import { db } from "~/db";
 
-export async function createContext({ req, resHeaders, info }: FetchCreateContextFnOptions) {
+export async function createContext({
+  req,
+  resHeaders,
+  info,
+}: FetchCreateContextFnOptions) {
   const session = await auth.api.getSession({
     headers: req.headers,
-  })
-  return { req, resHeaders, session, info, db }
+  });
+
+  return { req, resHeaders, session, info, db };
 }
 
-export type Context = Awaited<ReturnType<typeof createContext>>
+export type Context = Awaited<ReturnType<typeof createContext>>;
